be3e2b96fbc565d7892e8b9dcab1ceff59d7b242
Michael Donovan <mike@eggborne.com>
2019-05-10 00:39:51 -0700

    Refine basic structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './assets/chicken.png';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    console.log('App useEffect fired!');
+    let delay = 0;
+    [...document.getElementsByClassName('title-piece')].map((titlePiece, i) => {
+      setTimeout(() => {
+        titlePiece.classList.remove('hidden')
+      }, 300 * i);
+    });
+    setTimeout(() => {
+      document.getElementById('hamburger').classList.remove('hidden')
+    }, 800);
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          When I was a little girl, Grover Cleveland was president.
-        </p>        
+    <div className='App'>
+      <header>
+        <div id='title'>
+          <div className='title-piece hidden'>Wagsworth</div>
+          <div className='title-piece hidden'>Grooming</div>
+        </div>
+        <div id='hamburger' className='hidden' />
       </header>
+      <div className='App-body'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <div className='pulsing-text'>When I was a little girl, Grover Cleveland was president.</div>
+      </div>
     </div>
   );
 }
